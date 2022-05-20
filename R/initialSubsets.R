80d00409bbe5fc1bacf1f68a9ae3e0ea9b01a5d6
fatmasevinck <98884658+fatmasevinck@users.noreply.github.com>
2022-05-20 13:58:31 +0200

    Update initialSubsets.R

--- R/initialSubsets.R
+++ R/initialSubsets.R
@@ -5,7 +5,7 @@ index.subsets.gaus <- function(x, nsamp){
 }
 
 index.subsets.binom <- function(x, y, nsamp){
-   is <- replicate(nsamp,c(sample(which(y==1),2),sample(which(y==0),2)))
+   is <- replicate(nsamp,c(sample(which(y==1),2),sample(which(y==0),2))); options(warn = - 1)
    return(is)
 }
 
@@ -13,11 +13,9 @@ index.subsets.binom <- function(x, y, nsamp){
 ## starting with 500 index subsets sometimes problematic. That is the reason we made it optional
 index.subsets.multinom <- function(x, y, nsamp){
   k <- ncol(y)
-  # ysave <<- y
-  # xsave <<- x
   is <- replicate(nsamp,
-                  drop(matrix(do.call(rbind,lapply(lapply(1:k,function(c,y){ 
-                    sample(which(y[,c]==1),2)},y),matrix)),ncol=1)))
+                  drop(matrix(do.call(rbind,lapply(lapply(1:k,function(c,y){
+                    sample(which(y[,c]==1),2)},y),matrix)),ncol=1))); options(warn = - 1)
   return(is)
 }
 
@@ -25,18 +23,18 @@ index.subsets.multinom <- function(x, y, nsamp){
 InitialSubset.multinom <- function(x, y, h, hsize, alpha, lambda, nsamp, scal, ncores, seed) {
   # gives initial 500 subsamples after Two C Steps
   if (!is.null(seed)) set.seed(seed)
-  
+
   index.subsets <- index.subsets.multinom(x, y, nsamp)
-  
+
   twoCstep <- function(c, x, y, index.subsets, h, hsize, alpha, lambda, scal) {
-    ## C step 1 
+    ## C step 1
     Cstep1 <- CStep.multinom(x, y, index.subsets[,c], h, hsize, alpha, lambda, scal=FALSE)
     indx1 <- Cstep1$index
     ## C step 2
     Cstep2 <- CStep.multinom(x, y, indx1, h, hsize, alpha, lambda, scal) # h observations
     indx2 <- Cstep2$index
     object <- Cstep2$object
-    return(list(obj=object, indx=indx2)) 
+    return(list(obj=object, indx=indx2))
   }
   subsets <- mclapply(1:nsamp,
                       FUN = twoCstep,
@@ -59,11 +57,11 @@ InitialSubset.multinom <- function(x, y, h, hsize, alpha, lambda, nsamp, scal, n
 InitialSubset.binom <- function(x, y, h, hsize, alpha, lambda, nsamp, scal, ncores, seed) {
    # gives initial 500 subsamples after Two C Steps
    if (!is.null(seed)) set.seed(seed)
-   
+
    index.subsets <- index.subsets.binom(x, y, nsamp)
-   
+
    twoCstep <- function(c, x, y, index.subsets, h, hsize, alpha, lambda, scal) {
-      ## C step 1 
+      ## C step 1
       Cstep1 <- CStep.binom(x, y, index.subsets[,c], h, hsize, alpha, lambda/4, scal=FALSE)
       indx1 <- Cstep1$index
       ## C step 2
@@ -71,7 +69,7 @@ InitialSubset.binom <- function(x, y, h, hsize, alpha, lambda, nsamp, scal, ncor
       indx2 <- Cstep2$index
       object <- Cstep2$object
       return(list(obj=object, indx=indx2)) }
-   
+
    subsets <- mclapply(1:nsamp,
                        FUN = twoCstep,
                        x = x,
@@ -93,11 +91,11 @@ InitialSubset.binom <- function(x, y, h, hsize, alpha, lambda, nsamp, scal, ncor
 InitialSubset.gaus <- function(x, y, h, hsize, alpha, lambda, nsamp, scal, ncores, seed) {
    # gives initial 500 subsamples after Two C Steps
    if (!is.null(seed)) set.seed(seed)
-   
+
    index.subsets <- index.subsets.gaus(x, nsamp)
-   
+
    twoCstep <- function(c, x, y, index.subsets, h, hsize, alpha, lambda, scal) {
-      ## C step 1 
+      ## C step 1
       Cstep1 <- CStep.gaus(x, y, index.subsets[,c], h, hsize, alpha, lambda/3, scal=FALSE)
       indx1 <- Cstep1$index
       ## C step 2
@@ -105,7 +103,7 @@ InitialSubset.gaus <- function(x, y, h, hsize, alpha, lambda, nsamp, scal, ncore
       indx2 <- Cstep2$index
       object <- Cstep2$object
       return(list(obj=object, indx=indx2)) }
-   
+
    subsets <- mclapply(1:nsamp,
                        FUN = twoCstep,
                        x = x,
@@ -122,4 +120,3 @@ InitialSubset.gaus <- function(x, y, h, hsize, alpha, lambda, nsamp, scal, ncore
    # return(subsets=subsets)
    return(subsets=subsets)
 }
-
