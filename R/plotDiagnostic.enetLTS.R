6fc51d36942205bd666d03bcc90f7350b30836c5
Fatma Sevinc KURNAZ <98884658+fatmasevinck@users.noreply.github.com>
2022-05-25 14:04:10 +0200

    Update plotDiagnostic.enetLTS.R

--- R/plotDiagnostic.enetLTS.R
+++ R/plotDiagnostic.enetLTS.R
@@ -143,7 +143,8 @@ plotDiagnostic.enetLTS <- function(object,vers=c("reweighted","raw"),...){
             axis.text.y  = element_text(size = 13),
             axis.text.x  = element_text(size = 13),
             legend.text  = element_text(size = 13),
-            legend.title = element_text(size = 13)) +
+            legend.title = element_text(size = 13),
+            legend.position= "bottom") +
       scale_colour_discrete(name="Diagnostics",breaks=c("1", "0"), labels=lab.val) +
       scale_shape_manual(values=c(4, 17),name="Diagnostics",breaks=c("0", "1"), labels=lab.val)
     print(plot)
